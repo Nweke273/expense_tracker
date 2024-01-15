899c81df779a5dda90df9678d1770a668f607b6d
Nweke273 <chisom5710@gmail.com>
2024-01-15 01:32:02 -0800

    Feat: Implement dark Mode

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,24 @@ import 'package:flutter/material.dart';
 
 var kColorScheme =
     ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 96, 80, 65));
+
+var kDarkColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 5, 99, 125));
 void main() {
   runApp(MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: kDarkColorScheme,
+        brightness: Brightness.dark,
+        cardTheme: const CardTheme().copyWith(
+          margin: const EdgeInsets.symmetric(horizontal: 18, vertical: 6),
+          color: kDarkColorScheme.secondaryContainer,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+                backgroundColor: kDarkColorScheme.primaryContainer,
+                foregroundColor: kDarkColorScheme.onPrimaryContainer)),
+      ),
       theme: ThemeData().copyWith(
           useMaterial3: true,
           scaffoldBackgroundColor: const Color.fromARGB(255, 255, 255, 255),
@@ -25,5 +41,6 @@ void main() {
                   fontWeight: FontWeight.bold,
                   color: kColorScheme.onSecondaryContainer,
                   fontSize: 18))),
+      themeMode: ThemeMode.system,
       home: const Expenses()));
 }
